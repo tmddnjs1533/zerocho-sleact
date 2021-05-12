64790e3366f8f2091df9a6a39d7e88c0b06c59ed
Kim Seoungwon <sw91@humanf.co.kr>
2021-05-12 09:39:13 +0900

    first commit

---
+++ setting/ts/client.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from '@layouts/App';
+import { BrowserRouter } from 'react-router-dom';
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#app'),
+);
+
+// pages - 서비스 페이지
+// components - 자잘한 컴포넌트
+// layouts - 공통 레이아웃
---
+++ setting/ts/layouts/App.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+
+const App: FC = () => {
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
+};
+
+export default App;
